e58b5b18781a8e9fb87aa65c011df4c00535503b
Kopano Monyobo <59963220+kopano-m@users.noreply.github.com>
2020-02-04 10:23:55 +0200

    Removed unnecessary text

--- Analyse_Functions_Student-1519.py
+++ Analyse_Functions_Student-1519.py
@@ -1,50 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-import numpy as np
-import pandas as pd
-
-
-# In[2]:
-
-
-url = 'https://raw.githubusercontent.com/RidhaMoosa/eskom_data-/master/electrification_by_province.csv'
-ebp = pd.read_csv(url)
-
-for col, row in ebp.iloc[:,1:].iteritems():
-    ebp[col] = ebp[col].str.replace(',','').astype(int)
-
-limpopo = ebp['Limpopo'].to_list()
-limpopo = [float(x) for x in limpopo]
-
-mpumalanga = ebp['Mpumalanga'].to_list()
-mpumalanga = [float(x) for x in mpumalanga]
-
-north_west = ebp['North west'].to_list()
-north_west = [float(x) for x in north_west]
-
-free_state = ebp['Free State'].to_list()
-free_state = [float(x) for x in free_state]
-
-kwazulu_natal = ebp['Kwazulu Natal'].to_list()
-kwazulu_natal = [float(x) for x in kwazulu_natal]
-
-eastern_cape = ebp['Eastern Cape'].to_list()
-eastern_cape = [float(x) for x in eastern_cape]
-
-western_cape = ebp['Western Cape'].to_list()
-western_cape = [float(x) for x in western_cape]
-
-northern_cape = ebp['Northern Cape'].to_list()
-northern_cape = [float(x) for x in northern_cape]
-
-gauteng = ebp['Gauteng'].to_list()
-gauteng = [float(x) for x in gauteng]
-
-url = 'https://raw.githubusercontent.com/RidhaMoosa/eskom_data-/master/twitter_nov_2019.csv'
-twitter_df = pd.read_csv(url)
-
-dates = twitter_df['Date'].to_list()
-
 def dictionary_of_metrics(data):
 
     mean = lambda data : sum(data)/len(data)
